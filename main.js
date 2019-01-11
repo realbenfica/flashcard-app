d546ee91a1cc4bd65b3ac7dc91155e18dd6a6d36
“ALEX <alexgtr84@gmail.com>
2019-01-11 15:42:57 +0100

    final state

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
-function sky() {
-    document.getElementById("question1").innerText = "Blue"
-    
+let currentQuestionNumber = 0
+const questions = ["What is the colour of the sky?", " is the meaning of life", "How deep is the ocean?"]
+const answers = ["Blue", "Everything", "Very deep"]
+
+function displayCurrentQuestion() {
+    document.getElementById("questions").innerText = questions[currentQuestionNumber];
 }
 
+function nextQuestion() {
+    currentQuestionNumber++;
+    displayCurrentQuestion();
+    displayBeginAnswer();
+}
+
+displayCurrentQuestion()
 
 
+function displayCurrentAnswer() {
+    document.getElementById("answers").innerHTML = answers[currentQuestionNumber];
+}
+displayCurrentAnswer();
+
+
+function displayBeginAnswer() {
+    document.getElementById("answers").innerHTML = null;
+}
+displayBeginAnswer();
+
+
+
+
+// unction displayBeginAnswer() {
+//     if(answers[0]){
+//         document.getElementById("answers").innerHTML = null;
+//         {
+//         else { 
+//         document.getElementById("answers").innerHTML = answers[currentQuestionNumber];
+//         }
+// }
+// displayBeginAnswer();
+
+
+
+// function nav() {
+//     document.getElementById("questions").innerText = "What is the meaning of life?"; 
+    // const newDiv = document.createElement("div");  
+    // newDiv.className = "question2";   
+    // section.appendChild(newDiv);
+    // document.getElementsByClassName("question2")[0]
+    
+// }
 
 // Flashcard
 // Step 1  html with a title and a question with a button to reveal an answer.
@@ -17,4 +61,16 @@ function sky() {
 //         What we want to do:
 //         - we want to see a question with a button next or under it. 
 //         - we want the answer to be revealed when you click the button.
+
+// Intermediate steps
+// Connect colaborator on github
+// clone repository on Lai's terminal
+// test pull and push functions from Lai's git
+// put repository online
+
+// Step 2     create Navigation
+//             - create 1 button "next"
+//             - create new question
+//             - create interaction where pressing next shows new question
+// Step 3      create an array with questions and iterate this array in the question BoxBrowser
 
