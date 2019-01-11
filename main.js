1cffb23c9990f6bad1fe3ce015a708b09bab2ff4
“ALEX <alexgtr84@gmail.com>
2019-01-11 11:19:44 +0100

    first flashcard

---
+++ main.js
@@ -0,0 +1,20 @@
+function sky() {
+    document.getElementById("question1").innerText = "Blue"
+    
+}
+
+
+
+
+// Flashcard
+// Step 1  html with a title and a question with a button to reveal an answer.
+//         - create html file
+//         - create js file
+//         - Hardcode the question in the html
+//         - create a button in js/html
+//         - in the js code the answer in the function (event handler)
+
+//         What we want to do:
+//         - we want to see a question with a button next or under it. 
+//         - we want the answer to be revealed when you click the button.
+
